08692c9e8be36bfc08e3c4114d534d804dee8318
altinawiwaseem <wasemm@live.com>
2023-05-09 01:20:24 +0200

    create a listingController

---
+++ app/Http/Controllers/ListingController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ListingController extends Controller
+{
+    // show all listings
+    public function index(){
+
+    }
+
+    // show single listing
+    public function show(){
+
+    }
+}
