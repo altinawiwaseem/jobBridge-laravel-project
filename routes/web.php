b9222be8bf121763307d049da4187351fdc30d5e
altinawiwaseem <wasemm@live.com>
2023-05-09 01:23:37 +0200

    moving the route functionality into controller

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ListingController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Models\Listing;
@@ -17,21 +18,9 @@ use App\Models\Listing;
 
 // All Listings
 
-Route::get('/', function () {
-    return view('listings', [
-        "heading" => " Latest Listings ", 
-        "listings" => Listing::all()
-
-    ]);
-});
+Route::get('/', [ListingController::class, "index"]);
 
 // Single Listing 
 
 // route model binding
-Route::get("/listings/{listing}", function(Listing $listing){
-
-return view("listing", [
-    "listing" => $listing
-]);
-
-});
+Route::get("/listings/{listing}", [ListingController::class, "show"]);
